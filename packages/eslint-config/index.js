a2cd477d645769c5328dd47c8a500a03c1e25eee
pjh <pjh@twolinecode.com>
2024-05-23 16:16:30 +0900

    update rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,15 +3,39 @@ module.exports = {
     '@rushstack/eslint-config/profile/web-app',
   ],
   rules: {
-    "@typescript-eslint/naming-convention": [
-      "warn",
+    // error
+    'no-eval': 'error',
+
+    // warn
+    semi: ['warn', 'never'],
+    quotes: ['warn', 'single'],
+    eqeqeq: ['warn', 'always'],
+    indent: ['warn', 2],
+    'rest-spread-spacing': ['warn', 'never'],
+    'space-infix-ops': 'warn',
+    'func-style': ['warn', 'expression'],
+    'no-unneeded-ternary': 'warn',
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: 'directive', next: '*' },
+      { blankLine: 'any', prev: 'directive', next: 'directive' }
+    ],
+    'space-before-function-paren': ['warn', 'never'],
+    'keyword-spacing': ['warn', { before: true, after: true }],
+    'eol-last': ['warn', 'always'],
+    'no-multiple-empty-lines': ['warn', { max: 2 }],
+    '@typescript-eslint/naming-convention': [
+      'warn',
       {
-        "selector": "parameter",
-        "format": [
-          "camelCase"
+        selector: 'parameter',
+        format: [
+          'camelCase'
         ],
-        "leadingUnderscore": "allow"
+        leadingUnderscore: 'allow'
       }
     ]
-  },
+  }
 };
